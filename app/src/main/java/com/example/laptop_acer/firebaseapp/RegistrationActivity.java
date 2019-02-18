a19bc8721ada0a61a65c3b9465cd1a95ddb005bc
unknown <you@example.com>
2019-02-18 15:34:44 +0200

    check already exist email

--- app/src/main/java/com/example/laptop_acer/firebaseapp/RegistrationActivity.java
+++ app/src/main/java/com/example/laptop_acer/firebaseapp/RegistrationActivity.java
@@ -1,11 +1,9 @@
 package com.example.laptop_acer.firebaseapp;
 
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.design.widget.TextInputLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.util.Patterns;
 import android.view.View;
@@ -16,28 +14,22 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthUserCollisionException;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.ProviderQueryResult;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
 
 public class RegistrationActivity extends AppCompatActivity {
 
     private DatabaseReference databaseReference;
     private FirebaseAuth firebaseAuth;
-    private ProgressDialog progressDialog;
-//    private ProgressBar progressBar;
+    private ProgressBar progressBar;
     private ImageView imageViewReg;
-    private EditText editTextNickname;
-    private EditText editTextEmail;
-    private EditText editTextPassword;
-    private EditText editTextConfirmPassword;
+    private EditText inputTextNickname;
+    private EditText inputTextEmail;
+    private EditText inputTextPassword;
+    private EditText inputTextConfirmPassword;
     private Button buttonRegistration;
 
 
@@ -46,85 +38,79 @@ public class RegistrationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
+        firebaseAuth = FirebaseAuth.getInstance();
 
         imageViewReg = findViewById(R.id.img_vw_reg);
-        editTextNickname = findViewById(R.id.edt_txt_nickname);
-        editTextEmail = findViewById(R.id.edt_txt_email);
-        editTextPassword = findViewById(R.id.edt_txt_password);
-        editTextConfirmPassword = findViewById(R.id.edt_txt_confirm_password);
+        inputTextNickname = findViewById(R.id.edt_txt_nickname);
+        inputTextEmail = findViewById(R.id.edt_txt_email);
+        inputTextPassword = findViewById(R.id.edt_txt_password);
+        inputTextConfirmPassword = findViewById(R.id.edt_txt_confirm_password);
         buttonRegistration = findViewById(R.id.btn_registration);
-
-        firebaseAuth = FirebaseAuth.getInstance();
-        progressDialog = new ProgressDialog(this);
+        progressBar = findViewById(R.id.progressbar);
 
         buttonRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nickname = editTextNickname.getText().toString().trim();
-                String email = editTextEmail.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
-                String confirmPassword = editTextConfirmPassword.getText().toString().trim();
+                String nickname = inputTextNickname.getText().toString().trim();
+                String email = inputTextEmail.getText().toString().trim();
+                String password = inputTextPassword.getText().toString().trim();
+                String confirmPassword = inputTextConfirmPassword.getText().toString().trim();
 
                 if (nickname.isEmpty()) {
-                    editTextNickname.setError("Nickname is required");
-                    editTextNickname.requestFocus();
+                    inputTextNickname.setError("Nickname is required");
+                    inputTextNickname.requestFocus();
                     return;
                 }
                 if (email.isEmpty()) {
-                    editTextEmail.setError("Email is required");
-                    editTextEmail.requestFocus();
+                    inputTextEmail.setError("Email is required");
+                    inputTextEmail.requestFocus();
                     return;
                 }
 
                 if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    editTextEmail.setError("Please enter a valid email");
-                    editTextEmail.requestFocus();
+                    inputTextEmail.setError("Please enter a valid email");
+                    inputTextEmail.requestFocus();
                     return;
                 }
 
                 if (password.isEmpty()) {
-                    editTextPassword.setError("Password is required");
-                    editTextPassword.requestFocus();
+                    inputTextPassword.setError("Password is required");
+                    inputTextPassword.requestFocus();
                     return;
                 }
 
                 if (password.length() < 6) {
-                    editTextPassword.setError("Minimum 6 symbols");
-                    editTextPassword.requestFocus();
+                    inputTextPassword.setError("Minimum 6 symbols");
+                    inputTextPassword.requestFocus();
                     return;
                 }
 
                 if (!confirmPassword.equals(password)) {
-                    editTextConfirmPassword.setError("The password not match");
-                    editTextConfirmPassword.requestFocus();
+                    inputTextConfirmPassword.setError("The password not match");
+                    inputTextConfirmPassword.requestFocus();
                     return;
                 }
 
                 if (!TextUtils.isEmpty(nickname) && !TextUtils.isEmpty(email)
                         && !TextUtils.isEmpty(password) && !TextUtils.isEmpty(confirmPassword)) {
-                    progressDialog.setTitle("Loading...");
-                    progressDialog.setMessage("Please wait...");
-                    progressDialog.setCanceledOnTouchOutside(true);
-                    progressDialog.show();
                     register(nickname, email, password, confirmPassword);
-
                 }
 
             }
 
             private void register(final String nickname, final String email, final String password,
                                   final String confirmPassword) {
+
+                progressBar.setVisibility(View.VISIBLE);
                 firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
-                        Toast.makeText(RegistrationActivity.this, "createUserWithEmail:onComplete:" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
-//                        progressBar.setVisibility(View.GONE);
-
+                        progressBar.setVisibility(View.GONE);
                         if (!task.isSuccessful()) {
-                            Toast.makeText(RegistrationActivity.this, "Authentication failed." + task.getException(),
-                                    Toast.LENGTH_SHORT).show();
+                            checkEmailAlreadyExist();
                         } else {
                             startActivity(new Intent(RegistrationActivity.this, LoginActivity.class));
+                            Toast.makeText(getApplicationContext(), "Congratulations, you  have new registration!", Toast.LENGTH_SHORT).show();
                             finish();
                         }
                     }
@@ -132,7 +118,32 @@ public class RegistrationActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void checkEmailAlreadyExist() {
+        firebaseAuth.fetchProvidersForEmail(inputTextEmail.getText().toString())
+                .addOnCompleteListener(new OnCompleteListener<ProviderQueryResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<ProviderQueryResult> task) {
+
+                        boolean check = !task.getResult().getProviders().isEmpty();
+
+                        if (!check) {
+                            Toast.makeText(getApplicationContext(), "Email already exist", Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Email already exist", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        progressBar.setVisibility(View.GONE);
+    }
 }
+
 
 
 
