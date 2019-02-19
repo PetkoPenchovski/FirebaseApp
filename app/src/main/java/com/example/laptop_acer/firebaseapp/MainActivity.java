8c9f24faa65e16e7fc6d7bf9a056f8ca6b667177
unknown <you@example.com>
2019-02-19 11:56:21 +0200

    added bottom navigation fragmnets

--- app/src/main/java/com/example/laptop_acer/firebaseapp/MainActivity.java
+++ app/src/main/java/com/example/laptop_acer/firebaseapp/MainActivity.java
@@ -1,24 +1,87 @@
 package com.example.laptop_acer.firebaseapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
-import com.google.firebase.auth.FirebaseAuth;
+import com.example.laptop_acer.firebaseapp.fragments.AccountFragment;
+import com.example.laptop_acer.firebaseapp.fragments.DescriptionFragment;
+import com.example.laptop_acer.firebaseapp.fragments.HomeFragment;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private FirebaseAuth mAuth;
+public class MainActivity extends AppCompatActivity {
+
+//    private ViewPager viewPager;
+    private TabLayout tabLayout;
+//    private PagerAdapter PagerAdapter;
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//        viewPager = findViewById(R.id.view_pager);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_view);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new HomeFragment()).commit();
+//        tabLayout = findViewById(R.id.tab_layout);
+
+//        tabLayout.addTab(tabLayout.newTab().setText("1"));
+//        tabLayout.addTab(tabLayout.newTab().setText("2"));
+//        tabLayout.addTab(tabLayout.newTab().setText("3"));
+//        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+//        PagerAdapter = new PagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+//        viewPager.setAdapter(PagerAdapter);
+//
+//        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+//        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+//            @Override
+//            public void onTabSelected(TabLayout.Tab tab) {
+//                viewPager.setCurrentItem(tab.getPosition());
+//            }
+//
+//            @Override
+//            public void onTabUnselected(TabLayout.Tab tab) {
+//
+//            }
+//
+//            @Override
+//            public void onTabReselected(TabLayout.Tab tab) {
+//
+//            }
+//        });
+//
+//    }
+//
     }
 
-    @Override
-    public void onClick(View v) {
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
 
-    }
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_description:
+                            selectedFragment = new DescriptionFragment();
+                            break;
+                        case R.id.nav_account:
+                            selectedFragment = new AccountFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
